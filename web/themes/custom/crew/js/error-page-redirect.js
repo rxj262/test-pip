425e19b5aebb9b8afd2b3dff6bdb48e55bcaf94c
Cheng Chen <cxc891@case.edu>
2024-10-30 10:28:23 -0400

    updated js

--- web/themes/custom/crew/js/error-page-redirect.js
+++ web/themes/custom/crew/js/error-page-redirect.js
@@ -2,13 +2,16 @@
   'use strict';
   Drupal.behaviors.error_page_redirect = {
     attach(context, drupalSettings) {
-      if (drupalSettings.labs_custom_blocks.homepage_redirect_url != null) {
-        setTimeout(
-          function () {
-            window.location.href = drupalSettings.labs_custom_blocks.homepage_redirect_url;
-          }, 5000,
-        );
-      } else {
+      if (typeof drupalSettings.labs_custom_blocks !== 'undefined') {
+        if (drupalSettings.labs_custom_blocks.homepage_redirect_url != null) {
+          setTimeout(
+            function () {
+              window.location.href = drupalSettings.labs_custom_blocks.homepage_redirect_url;
+            }, 5000,
+          );
+        }
+      }
+      else {
         setTimeout(
           function () {
             window.location = drupalSettings.crew.error_page.base_url;
