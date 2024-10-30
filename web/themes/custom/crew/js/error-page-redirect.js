5860b6412f569f4a8fb8aefe19f0f1e9f69a33de
Cheng Chen <cxc891@case.edu>
2024-10-30 09:23:45 -0400

    moved error page redirect to its own library

---
+++ web/themes/custom/crew/js/error-page-redirect.js
@@ -0,0 +1,20 @@
+(function ($, Drupal, once, drupalSettings) {
+  'use strict';
+  Drupal.behaviors.error_page_redirect = {
+    attach(context, drupalSettings) {
+      if (drupalSettings.labs_custom_blocks.homepage_redirect_url != null) {
+        setTimeout(
+          function () {
+            window.location.href = drupalSettings.labs_custom_blocks.homepage_redirect_url;
+          }, 5000,
+        );
+      } else {
+        setTimeout(
+          function () {
+            window.location = drupalSettings.crew.error_page.base_url;
+          }, 5000,
+        );
+      }     // modify drop icon default link
+    },
+  };
+})(jQuery, Drupal, once, drupalSettings);
